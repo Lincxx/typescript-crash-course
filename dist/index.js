89edb2094ca2b5fc9c0b15d931eadc2077665cb1
lincxx <bossc@BOSSCO>
2024-02-26 11:45:31 -0500

    Finished #6

--- dist/index.js
+++ dist/index.js
@@ -22,35 +22,69 @@
 // tsc --watch
 //Now for Node - watching a file
 //node --watch dist/index.js
-//#5
+//---------#5
 //--- Arrays
-let names = ["lee", "bobby", "fred"];
-let ages = [1, 2, 3, 4];
-//push new values
-names.push("jeremy");
-ages.push(23);
-//---- type inference with arrays
-let fruits = ['apple', 'pears', 'grapes', 'mangoes'];
-//fruits.push(true) // error
-fruits.push('peaches');
-const f = fruits[3];
-//this is called a union type
-let things = [1, true, 'hello'];
-const t = things[0];
-//--- object literals
-let user = {
-    firstName: "Mario",
-    age: 43,
-    id: 12312
+// let names: string[] = ["lee", "bobby", "fred"]
+// let ages: number[] = [1,2,3,4]
+//
+// //push new values
+// names.push("jeremy")
+// ages.push(23)
+//
+// //---- type inference with arrays
+// let fruits = ['apple', 'pears', 'grapes', 'mangoes']
+//
+// //fruits.push(true) // error
+// fruits.push('peaches')
+//
+// const f = fruits[3]
+//
+// //this is called a union type
+// let things = [1,true, 'hello']
+// const t = things[0]
+//
+// //--- object literals
+// let user: {firstName: string, age: number, id: number} = {
+//     firstName: "Mario",
+//     age: 43,
+//     id: 12312
+// }
+//
+// //user.firstName = 324//error
+//
+// //add property
+// //user.email = "w@mail.com" //error
+//
+// //---- type inference w/ objects literals
+// let person = {
+//     name: 'luigi',
+//     age: 35
+// }
+//
+// //person.name = true //error
+// //person.age = 45
+//
+// const p = person.age
+//---------#6 Functions
+// functions
+function add(a, b) {
+    return a + b;
+}
+//add('a', true)// error
+add(1, 2);
+const sub = (a, b) => {
+    return a - b;
 };
-//user.firstName = 324//error
-//add property
-//user.email = "w@mail.com" //error
-//---- type inference w/ objects literals
-let person = {
-    name: 'luigi',
-    age: 35
-};
-//person.name = true //error
-//person.age = 45
-const p = person.age;
+sub(2, 3);
+//void - means nothing is going to be return
+function addAll(items) {
+    const total = items.reduce((a, c) => a + c, 0);
+    console.log(total);
+}
+addAll([1, 2, 3]);
+//returntype inference
+function formatGreeting(name, greeting) {
+    return `${greeting}, ${name}`;
+}
+const result = formatGreeting('jeremy', 'Hello');
+console.log(result);
--- src/index.ts
+++ src/index.ts
@@ -23,47 +23,78 @@
 //Now for Node - watching a file
 //node --watch dist/index.js
 
-//#5
+//---------#5
 //--- Arrays
-let names: string[] = ["lee", "bobby", "fred"]
-let ages: number[] = [1,2,3,4]
+// let names: string[] = ["lee", "bobby", "fred"]
+// let ages: number[] = [1,2,3,4]
+//
+// //push new values
+// names.push("jeremy")
+// ages.push(23)
+//
+// //---- type inference with arrays
+// let fruits = ['apple', 'pears', 'grapes', 'mangoes']
+//
+// //fruits.push(true) // error
+// fruits.push('peaches')
+//
+// const f = fruits[3]
+//
+// //this is called a union type
+// let things = [1,true, 'hello']
+// const t = things[0]
+//
+// //--- object literals
+// let user: {firstName: string, age: number, id: number} = {
+//     firstName: "Mario",
+//     age: 43,
+//     id: 12312
+// }
+//
+// //user.firstName = 324//error
+//
+// //add property
+// //user.email = "w@mail.com" //error
+//
+// //---- type inference w/ objects literals
+// let person = {
+//     name: 'luigi',
+//     age: 35
+// }
+//
+// //person.name = true //error
+// //person.age = 45
+//
+// const p = person.age
 
-//push new values
-names.push("jeremy")
-ages.push(23)
+//---------#6 Functions
 
-//---- type inference with arrays
-let fruits = ['apple', 'pears', 'grapes', 'mangoes']
-
-//fruits.push(true) // error
-fruits.push('peaches')
-
-const f = fruits[3]
-
-//this is called a union type
-let things = [1,true, 'hello']
-const t = things[0]
-
-//--- object literals
-let user: {firstName: string, age: number, id: number} = {
-    firstName: "Mario",
-    age: 43,
-    id: 12312
+// functions
+function add(a: number,b: number): number{
+    return a + b
 }
 
-//user.firstName = 324//error
+//add('a', true)// error
+add(1,2)
 
-//add property
-//user.email = "w@mail.com" //error
-
-//---- type inference w/ objects literals
-let person = {
-    name: 'luigi',
-    age: 35
+const sub = (a: number,b: number): number => {
+    return a - b
 }
 
-//person.name = true //error
-//person.age = 45
+sub(2,3)
 
-const p = person.age
+//void - means nothing is going to be return
+function addAll(items: number[]):void {
+    const total = items.reduce((a,c)=> a+c, 0)
+    console.log(total)
+}
 
+addAll([1,2,3])
+
+//return type inference
+function formatGreeting(name: string, greeting: string) {
+    return `${greeting}, ${name}`
+}
+
+const result = formatGreeting('jeremy', 'Hello')
+console.log(result)
