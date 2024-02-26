ec3a9784e9c1f64faddde27aa76235000ece5db0
lincxx <bossc@BOSSCO>
2024-02-26 10:47:24 -0500

    finished section 1-4

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+// let age: number = 30;
+// let firstName: string = 'Mario';
+// let isFictional: boolean
+//
+// let planet = 'Earth'
+// let moons = 1
+// let isLarge = false
+//
+// planet = 'Saturn'
+// moons = 145
+// isLarge = true
+//
+// //null & undefined
+// let something: null
+// let anotherThing: undefined
+//
+// something = null
+// anotherThing = undefined
+
+//#4
+//tsc --init - we changed the rootDir and output dir
+// tsc --watch
+//Now for Node - watching a file
+//node --watch dist/index.js
+
+//#5
+// Arrays
+let names: string[]
+
+//object literals
+
+//type inference w/ objects literals
